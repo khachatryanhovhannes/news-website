1b9c23e73fa8b32386ec168caba5805cad319971
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-08 10:23:55 +0400

    develop: Adding theme functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import { ThemeToggle } from "@/components/atoms";
 
 export default function Home() {
   return (
     <main>
-      <div className="min-h-screen bg-background-primary-light dark:bg-background-primary-dark">
-        <ThemeToggle />
+      <div className="min-h-screen ">
         <h1 className="text-4xl font-bold text-text-primary-light dark:text-text-primary-dark">
           Welcome to Tailwind CSS with Light & Dark Mode!
         </h1>
