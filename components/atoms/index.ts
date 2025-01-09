4d8c5115a920451b1187e5d0b1672956670acd0a
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-09 17:56:24 +0400

    develop: Adding banner

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -1,5 +1,6 @@
 import ThemeToggle from "./theme_toggle";
 import NavItem from "./nav_item";
 import Logo from "./logo";
+import BigBanner from "./big_banner";
 
-export { ThemeToggle, NavItem, Logo };
+export { ThemeToggle, NavItem, Logo, BigBanner };
