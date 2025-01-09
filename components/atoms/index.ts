74cb1cfb9ce8cb8e6f419d21b5d20fd75f687d3b
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-09 18:02:38 +0400

    develop: Adding social icons

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -2,5 +2,6 @@ import ThemeToggle from "./theme_toggle";
 import NavItem from "./nav_item";
 import Logo from "./logo";
 import BigBanner from "./big_banner";
+import SocialIcons from "./social_icons/index";
 
-export { ThemeToggle, NavItem, Logo, BigBanner };
+export { ThemeToggle, NavItem, Logo, BigBanner, SocialIcons };
---
+++ components/atoms/social_icons/index.tsx
@@ -0,0 +1,11 @@
+import { FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
+
+export default function SocialIcons() {
+  return (
+    <div className="text-3xl">
+      <FaFacebook />
+      <FaLinkedin />
+      <FaTwitter />
+    </div>
+  );
+}
