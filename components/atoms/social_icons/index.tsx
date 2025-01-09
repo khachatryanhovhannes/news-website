38d73bf6c853f31d5689f0186da7109ab824db5d
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-09 18:16:36 +0400

    develop: Adding styles menu

--- components/atoms/social_icons/index.tsx
+++ components/atoms/social_icons/index.tsx
@@ -1,11 +1,18 @@
+import Link from "next/link";
 import { FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 export default function SocialIcons() {
   return (
-    <div className="text-3xl">
-      <FaFacebook />
-      <FaLinkedin />
-      <FaTwitter />
+    <div className="text-3xl flex gap-2 text-background-secondary-dark dark:text-background-secondary-light">
+      <Link href="/" target="_blank">
+        <FaFacebook />
+      </Link>
+      <Link href="/" target="_blank">
+        <FaLinkedin />
+      </Link>
+      <Link href="/" target="_blank">
+        <FaTwitter />
+      </Link>
     </div>
   );
 }
