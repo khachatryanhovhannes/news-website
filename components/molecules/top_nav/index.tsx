e2ab3c5bd6a7560c8e608b9b4c6a9b309d88d63a
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-09 18:03:58 +0400

    develop: Creating top nav

---
+++ components/molecules/top_nav/index.tsx
@@ -0,0 +1,11 @@
+import { Logo, SocialIcons, ThemeToggle } from "@/components/atoms";
+
+export default function TopNav() {
+  return (
+    <div>
+      <Logo />
+      <SocialIcons />
+      <ThemeToggle />
+    </div>
+  );
+}
