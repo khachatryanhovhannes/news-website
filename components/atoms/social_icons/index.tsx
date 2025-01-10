af1d642894bdaf371e0b9c9ed995709f88a2c9c3
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-10 12:41:10 +0400

    develop: Change social icons color

--- components/atoms/social_icons/index.tsx
+++ components/atoms/social_icons/index.tsx
@@ -3,7 +3,7 @@ import { FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 export default function SocialIcons() {
   return (
-    <div className="text-3xl flex gap-2 text-background-secondary-dark dark:text-background-secondary-light">
+    <div className="text-3xl flex gap-2 ">
       <Link href="/" target="_blank">
         <FaFacebook />
       </Link>
