9c16f17eeb5673c49dba2377024fda307f710d80
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-13 20:46:39 +0400

    develop: Adding home page fix heading for development

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { PageHeading } from "@/components/atoms";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="h-screen">
       <PageHeading title="Վերջին տեխնոլոգիական նորությունները" />
     </main>
   );
