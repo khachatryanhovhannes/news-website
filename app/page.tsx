d207b4c5e68d3d37fa2a0aee32eef4f2f8309c80
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-13 20:44:09 +0400

    develop: Adding page heading in home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { PageHeading } from "@/components/atoms";
+
 export default function Home() {
   return (
     <main className="">
-     
+      <PageHeading title="Վերջին տեխնոլոգիական նորությունները" />
     </main>
   );
 }
