c31f166179b5dc29e814bb6608efa6d798d109b7
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-14 18:51:51 +0400

    develop: Creating home latest news banner small component

---
+++ components/molecules/home_latest_small/index.tsx
@@ -0,0 +1,3 @@
+export default function HomeLatestSmall() {
+  return <article></article>;
+}
--- components/molecules/index.ts
+++ components/molecules/index.ts
@@ -1,5 +1,6 @@
 import HomeLatestBig from "./home_latest_big";
 import Navbar from "./navbar";
 import TopNav from "./top_nav";
+import HomeLatestSmall from "./home_latest_small";
 
-export { Navbar, TopNav, HomeLatestBig };
+export { Navbar, TopNav, HomeLatestBig, HomeLatestSmall };
