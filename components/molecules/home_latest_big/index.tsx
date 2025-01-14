c57f878960b9f103b3ec8757446616659d0759ad
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-14 19:22:22 +0400

    develop: Develop Home big banner

--- components/molecules/home_latest_big/index.tsx
+++ components/molecules/home_latest_big/index.tsx
@@ -1,3 +1,11 @@
 export default function HomeLatestBig() {
-  return <article></article>;
+  return (
+    <article
+      className="w-full bg-white shadow-lg rounded-lg overflow-hidden bg-cover"
+      style={{
+        backgroundImage: "url('images/appleProduct.jpg')",
+      }}
+    >
+    </article>
+  );
 }
