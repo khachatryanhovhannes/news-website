e1e8b0091673f02e7ff862fd37e4e5c8a24936b3
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-14 18:50:59 +0400

    develop: Creating home latest news banner gib component

---
+++ components/molecules/home_latest_big/index.tsx
@@ -0,0 +1,3 @@
+export default function HomeLatestBig() {
+  return <article></article>;
+}
--- components/molecules/index.ts
+++ components/molecules/index.ts
@@ -1,4 +1,5 @@
+import HomeLatestBig from "./home_latest_big";
 import Navbar from "./navbar";
 import TopNav from "./top_nav";
 
-export { Navbar, TopNav };
+export { Navbar, TopNav, HomeLatestBig };
