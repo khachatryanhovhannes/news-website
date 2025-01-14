0f69e723c987d39df0f0ec0696ed5d5fc9afd93f
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-14 19:22:56 +0400

    develop: Adding layouts in home latest section

--- components/organisms/home_latest/index.tsx
+++ components/organisms/home_latest/index.tsx
@@ -1,1 +1,15 @@
-export default function HomeLatest() {}
+import { HomeLatestBig, HomeLatestSmall } from "@/components/molecules";
+
+export default function HomeLatest() {
+  return (
+    <section className="grid-cols-2 w-full grid gap-7">
+      <HomeLatestBig />
+      <section className="grid-cols-2 grid gap-7">
+        <HomeLatestSmall />
+        <HomeLatestSmall />
+        <HomeLatestSmall />
+        <HomeLatestSmall />
+      </section>
+    </section>
+  );
+}
