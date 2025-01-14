74fffc58de6e7485fd7751b0fbfd61430e55a962
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-14 19:22:35 +0400

    develop: Develop Home small

--- components/molecules/home_latest_small/index.tsx
+++ components/molecules/home_latest_small/index.tsx
@@ -1,3 +1,12 @@
 export default function HomeLatestSmall() {
-  return <article></article>;
+  return (
+    <article
+      className="w-full bg-white shadow-lg rounded-lg overflow-hidden bg-cover"
+      style={{
+        backgroundImage: "url('images/appleProduct.jpg')",
+        aspectRatio: "3 / 2",
+      }}
+    >
+    </article>
+  );
 }
