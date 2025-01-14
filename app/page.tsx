0924bbd1e10eb58034df4580bbd77a86789a8d96
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-14 19:20:54 +0400

    develop: Latest section in main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { PageHeading } from "@/components/atoms";
+import { HomeLatest } from "@/components/organisms";
 
 export default function Home() {
   return (
     <main className="h-screen">
       <PageHeading title="Վերջին տեխնոլոգիական նորությունները" />
+      <HomeLatest />
     </main>
   );
 }
