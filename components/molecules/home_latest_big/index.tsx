a97e69fd843d7afa5eaf251eec22c40eb93cf1e4
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-16 17:48:33 +0400

    bugfix: Fixing big banner

--- components/molecules/home_latest_big/index.tsx
+++ components/molecules/home_latest_big/index.tsx
@@ -1,69 +1,51 @@
-"use client";
 import Link from "next/link";
-import { useState } from "react";
+
+const info = {
+  title: "Apple Next-Generation AR Glasses",
+  desc: "Introducing the latest in AR glasses from Apple. These state-of-the-art devices will transform your gaming experience. Experience firsthand what's possible with Apple's revolutionary AR technology.",
+  keywords: [
+    "#Apple",
+    "#ARGlasses",
+    "#Augmented Reality",
+    "#Innovation",
+    "#TechNews",
+  ],
+  date: "10.01.2025",
+};
 
 export default function HomeLatestBig() {
-    const [title, setTitle] = useState("Apple Next-Generation AR Glasses Misha Misha Apple Next-Generation AR Glasses Misha MishaApple Next-Generation AR Glasses Misha MishaApple Next-Generation AR Glasses Misha MishaApple Next-Generation AR Glasses Misha MishaApple Next-Generation AR Glasses Misha MishaApple Next-Generation AR Glasses Misha Misha");
-    console.log(title, title.length);
-    const [desc, setDesc] = useState("Apple has announced its latest innovation: augmented reality (AR) glasses that seamlessly integrate digital information into the physical world. These glasses feature a lightweight design, high-resolution displays, and advanced sensors, aiming to revolutionize user interaction with technology. augmented reality (AR) glasses that seamlessly integrate digital information into the physical world. These glasses feature a lightweight design, high-resolution displays, and advanced sensors, aiming to revolutionize user interaction with technology.augmented reality (AR) glasses that seamlessly integrate digital information into the physical world. These glasses feature a lightweight design, high-resolution displays, and advanced sensors, aiming to revolutionize user interaction with technology.augmented reality (AR) glasses that seamlessly integrate digital information into the physical world. These glasses feature a lightweight design, high-resolution displays, and advanced sensors, aiming to revolutionize user interaction with technology.augmented reality (AR) glasses that seamlessly integrate digital information into the physical world. These glasses feature a lightweight design, high-resolution displays, and advanced sensors, aiming to revolutionize user interaction with technology.augmented reality (AR) glasses that seamlessly integrate digital information into the physical world. These glasses feature a lightweight design, high-resolution displays, and advanced sensors, aiming to revolutionize user interaction with technology.");
-    const [keywords, setKeywords] = useState("#Apple #ARGlasses #AugmentedReality #Innovation #TechNews");
-    const [date, setDate] = useState("10.01.2025");
-    const [showFullTitle, setShowFullTitle] = useState(false);
+  const { title, desc, keywords, date } = info;
+  return (
+    <article
+      className="w-full h-full bg-opacity-50 shadow-lg rounded-lg overflow-hidden bg-cover text-black dark:text-white"
+      style={{
+        backgroundImage: "url('images/appleProduct.jpg')",
+      }}
+    >
+      <div className="flex flex-col justify-between w-full h-full bg-opacity-60 bg-white dark:bg-black dark:bg-opacity-60">
+        <div className="flex flex-col items-end p-5">
+          <p className="rounded-lg mt-1 ">{date}</p>
+        </div>
 
-    const toggleFullTitle = () => {
-        setShowFullTitle(!showFullTitle);
-    }
-    return (
-        <article
-            className="w-full h-full bg-black bg-opacity-50 shadow-lg rounded-lg overflow-hidden bg-cover"
-            style={{
-                backgroundImage: "url('images/appleProduct.jpg')",
-            }}
-        >
-            <div className="flex flex-col justify-between w-full h-full bg-black bg-opacity-50">
-                <div className="flex flex-col items-end p-5">
-                    <p className="border-2 px-3 py-1 rounded-lg mt-1 bg-white text-black font-semibold">
-                        {date}
-                    </p>
-                </div>
-                <div className="flex flex-col justify-between p-5">
-                    <div className="pb-2 border-2 rounded-lg text-xl text-center pt-2 bg-white text-black">
-                        <Link href="https://google.com" className="text-blue-400 hover:underline">
-                            {keywords}
-                        </Link>
-
-                        {showFullTitle ? (
-                            <>
-                                <p>{title}</p>
-                                <button onClick={toggleFullTitle}>Show Less</button>
-                            </>
-                        ): (
-                            <>
-                                <p>{title.slice(0, 10)}</p>
-                                <button onClick={toggleFullTitle}>Read More</button>
-                            </>
-
-                        )}
-
-
-                        {/*{showFullTitle ? (*/}
-                        {/*    <>*/}
-                        {/*        <p>{title}</p>*/}
-                        {/*        <button onClick={toggleFullTitle} className="text-blue-400 hover:underline mt-2">*/}
-                        {/*            Show Less*/}
-                        {/*        </button>*/}
-                        {/*    </>*/}
-                        {/*) : (*/}
-                        {/*    <>*/}
-                        {/*        <p>{title.slice(0, 50)}...</p>*/}
-                        {/*        <button onClick={toggleFullTitle} className="text-blue-400 hover:underline mt-2">*/}
-                        {/*            Read More*/}
-                        {/*        </button>*/}
-                        {/*    </>*/}
-                        {/*)}*/}
-                    </div>
-                </div>
-            </div>
-        </article>
-    );
+        <div className="p-4">
+          <div className="flex gap-1">
+            {keywords.map((keyword, index) => (
+              <Link
+                href={`/search?q=${keyword}`}
+                key={index}
+                className="inline-block px-2 py-1 text-xs font-semibold tracking-wider bg-gradient-to-r from-green-400 to-blue-500 rounded-full"
+              >
+                {keyword}
+              </Link>
+            ))}
+          </div>
+          <h3 className="text-3xl mt-3">{title}</h3>
+          <p className="mt-1">
+            {desc.length > 120 ? desc.substring(0, 120) + "..." : desc}{" "}
+            <Link href="/">Read More</Link>
+          </p>
+        </div>
+      </div>
+    </article>
+  );
 }
