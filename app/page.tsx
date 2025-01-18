fd405f9dc3bd297b870152bc2c1ea12165d63c89
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-18 15:31:55 +0400

    develop: Adding home section in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { PageHeading } from "@/components/atoms";
-import { HomeLatest } from "@/components/organisms";
+import { HomeLatest, HomeSection } from "@/components/organisms";
 
 export default function Home() {
   return (
     <main className="">
       <PageHeading title="Վերջին տեխնոլոգիական նորությունները" />
       <HomeLatest />
+      <HomeSection title="Հայկական Նորություններ" />
     </main>
   );
 }
