3b859439dbb0a5975563646bcec06b6f3dee1f1f
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-18 17:40:09 +0400

    develop: Adding home sections desing

--- components/organisms/home_section/index.tsx
+++ components/organisms/home_section/index.tsx
@@ -1,12 +1,17 @@
+import { NewsList } from "@/components/molecules";
+import { INewsItem } from "@/models";
+
 interface IHomeSectionProps {
   title: string;
+  content: INewsItem[];
 }
 
 export default function HomeSection({ ...props }: IHomeSectionProps) {
-  const { title } = props;
+  const { title, content } = props;
   return (
-    <section>
-      <h3>{title}</h3>
+    <section className="my-8">
+      <h3 className="text-xl md:text-2xl">{title}</h3>
+      <NewsList news={content} />
     </section>
   );
 }
