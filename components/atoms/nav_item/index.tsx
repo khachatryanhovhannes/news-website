8b82f6fe9724678b207d1622ff7cfd1f312f106c
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-18 14:59:30 +0400

    develop: Fix moile nested icons

--- components/atoms/nav_item/index.tsx
+++ components/atoms/nav_item/index.tsx
@@ -8,34 +8,41 @@ interface INavItemProps {
   label: string;
   path: string;
   child?: INavItemProps[];
+  onClick?: () => void;
 }
 
 export default function NavItem({ ...props }: INavItemProps) {
-  const { label, path, child } = props;
+  const { label, path, child, onClick } = props;
   const [isOpen, setIsOpen] = useState(false);
   const pathname = usePathname();
   const isActive = pathname === path;
 
   return (
     <div className="relative w-full group">
-      <div className="cursor-pointer py-3">
+      <div className="cursor-pointer py-3 flex justify-center flex-row">
         <Link
-          className={`w-full ${isActive ? "underline underline-offset-4" : ""}`}
+          onClick={onClick}
+          className={` ${isActive ? "underline underline-offset-4" : ""}`}
           href={path}
         >
           <button className="block w-full hover:text-primary ">
             <div className="flex items-center justify-center">
               {label}
               {child && child.length > 0 && (
-                <FaCaretDown className="ml-2 transition-transform group-hover:rotate-180" />
+                <FaCaretDown className="ml-2 transition-transform group-hover:rotate-180 lg:block hidden" />
               )}
             </div>
           </button>
         </Link>
+        {child && child.length > 0 && (
+          <button>
+            <FaCaretDown className="ml-2 transition-transform group-hover:rotate-180 lg:hidden" />
+          </button>
+        )}
       </div>
 
       {child && child.length > 0 && (
-        <div className="pt-2 absolute top-full left-0 z-10 hidden group-hover:block rounded-lg shadow w-44">
+        <div className="pt-2 absolute top-full left-0 z-10 hidden lg:group-hover:block rounded-lg shadow w-44">
           <ul className="py-2 text-sm bg-background-secondary-light dark:bg-background-secondary-dark">
             {child.map((item, index) => (
               <li
@@ -44,18 +51,9 @@ export default function NavItem({ ...props }: INavItemProps) {
                 onMouseEnter={() => setIsOpen(!isOpen)}
               >
                 <Link href={item.path} className="block px-4 py-2">
-                  <button
-                    className="block w-full hover:text-primary"
-                    id="doubleDropdownButton"
-                    data-dropdown-toggle="doubleDropdown"
-                    data-dropdown-placement="right-start"
-                    type="button"
-                  >
+                  <button className="block w-full hover:text-primary">
                     <div className="flex items-center justify-center">
                       {item.label}
-                      {item.child && item.child.length > 0 && (
-                        <FaCaretDown className="ml-2 transition-transform group-hover:-rotate-90" />
-                      )}
                     </div>
                   </button>
                 </Link>
