867340154806c81c63c731652794247af309d6ff
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-18 15:34:16 +0400

    develop: Adding dump data for development

---
+++ dump/news.js
@@ -0,0 +1,37 @@
+export const NEWS = [
+  {
+    title: 'React Hooks: A Complete Guide',
+    date: '2022-01-01',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis velit id neque consectetur viverra. In malesuada sapien ac velit rutrum, at laoreet neque faucibus.',
+    path: "#",
+    image: '/images/news1.jpg',
+    keywords: ['news1', 'news',]
+  },
+
+  {
+    title: 'React Hooks: A Complete Guide',
+    date: '2022-01-01',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis velit id neque consectetur viverra. In malesuada sapien ac velit rutrum, at laoreet neque faucibus.',
+    path: "#",
+    image: '/images/news1.jpg',
+    keywords: ['news1', 'news',]
+  },
+
+  {
+    title: 'React Hooks: A Complete Guide',
+    date: '2022-01-01',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis velit id neque consectetur viverra. In malesuada sapien ac velit rutrum, at laoreet neque faucibus.',
+    path: "#",
+    image: '/images/news1.jpg',
+    keywords: ['news1', 'news',]
+  },
+
+  {
+    title: 'React Hooks: A Complete Guide',
+    date: '2022-01-01',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis velit id neque consectetur viverra. In malesuada sapien ac velit rutrum, at laoreet neque faucibus.',
+    path: "#",
+    image: '/images/news1.jpg',
+    keywords: ['news1', 'news',]
+  }
+]
