e0a7fb7e9cadbef685baf0e5601d8dde751f5065
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-18 14:47:06 +0400

    develop: Add header responsivity

--- components/organisms/header/index.tsx
+++ components/organisms/header/index.tsx
@@ -2,8 +2,8 @@ import { Navbar, TopNav } from "@/components/molecules";
 
 export default function Header() {
   return (
-    <header>
-      <div>
+    <header className="w-full">
+      <div className="w-full flex flex-row justify-between lg:flex-col ">
         <TopNav />
         <Navbar />
       </div>
