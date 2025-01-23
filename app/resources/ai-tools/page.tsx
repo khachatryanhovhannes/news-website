2a3beb5648a3c5ce95c41f09cf0a1f153315c793
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-23 11:29:24 +0400

    develop: Adding ai tools page content

--- app/resources/ai-tools/page.tsx
+++ app/resources/ai-tools/page.tsx
@@ -1,3 +1,17 @@
-export default function AiTools() {
-  return <>AI Tools</>;
+import { PageHeading } from "@/components/atoms";
+import Resource from "@/components/atoms/resource";
+import { getResources } from "@/services/api";
+
+export default async function AiTools() {
+  const resources = await getResources("ai-tools");
+  return (
+    <main className="px-4">
+      <PageHeading title="AI գործիքներ" />
+      <section className="grid grid-cols-4 gap-5">
+        {resources.map((resource) => (
+          <Resource resource={resource} key={resource.id} />
+        ))}
+      </section>
+    </main>
+  );
 }
