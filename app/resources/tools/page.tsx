d1e3465b71a30acf219a595a2853f49f86cae833
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-23 11:29:55 +0400

    develop: Adding tools page content

--- app/resources/tools/page.tsx
+++ app/resources/tools/page.tsx
@@ -1,3 +1,17 @@
-export default function Tools() {
-  return <>Tools</>;
+import { PageHeading } from "@/components/atoms";
+import Resource from "@/components/atoms/resource";
+import { getResources } from "@/services/api";
+
+export default async function Tools() {
+  const resources = await getResources("tools");
+  return (
+    <main className="px-4">
+      <PageHeading title="Գործիքներ" />
+      <section className="grid grid-cols-4 gap-5">
+        {resources.map((resource) => (
+          <Resource resource={resource} key={resource.id} />
+        ))}
+      </section>
+    </main>
+  );
 }
