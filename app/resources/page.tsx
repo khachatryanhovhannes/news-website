599577f16559698f7df2a337bb6f4b67fd5f432d
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-23 11:29:13 +0400

    develop: Adding resourses page content

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,3 +1,18 @@
-export default function Resources() {
-  return <main className="px-4"></main>;
+import { PageHeading } from "@/components/atoms";
+import Resource from "@/components/atoms/resource";
+import { getResources } from "@/services/api";
+
+export default async function Resources() {
+  const resources = await getResources();
+
+  return (
+    <main className="px-4">
+      <PageHeading title="Ռեսուրսներ" />
+      <section className="grid grid-cols-4 gap-5">
+        {resources.map((resource) => (
+          <Resource resource={resource} key={resource.id} />
+        ))}
+      </section>
+    </main>
+  );
 }
