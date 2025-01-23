debae395d718c576df41d328f55c07101f7d80d1
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-23 11:29:42 +0400

    develop: Adding educational page content

--- app/resources/educational-resources/page.tsx
+++ app/resources/educational-resources/page.tsx
@@ -1,3 +1,17 @@
-export default function EducationalResources() {
-  return <> Educational</>;
+import { PageHeading } from "@/components/atoms";
+import Resource from "@/components/atoms/resource";
+import { getResources } from "@/services/api";
+
+export default async function EducationalResources() {
+  const resources = await getResources("educational-resources");
+  return (
+    <main className="px-4">
+      <PageHeading title="Ուսումնական ռեսուրսներ" />
+      <section className="grid grid-cols-4 gap-5">
+        {resources.map((resource) => (
+          <Resource resource={resource} key={resource.id} />
+        ))}
+      </section>
+    </main>
+  );
 }
