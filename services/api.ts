dfd4ece66aa4a99b255232a1c8400b85c707ee6b
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-23 11:07:40 +0400

    bugfix: fix function props

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 import { resources } from "@/dump/resources";
 
-export const getResources = async (page: string | undefined) => {
+export const getResources = async (page?: string) => {
   if (page) {
     return resources.filter((res) => res.page === page);
   }
